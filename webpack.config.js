8dc72d8588a14c1b6f168c7b444786771368f090
ADAllen <1007715170@qq.com>
2019-01-14 17:14:31 +0800

    webpackdemo

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader"
+      }, {
+        loader: "sass-loader",
+        options: {
+          includePaths: ["absolute/path/a", "absolute/path/b"]
+        }
+      }]
+    }]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [require('@babel/plugin-transform-object-rest-spread')]
+          }
+        }
+      }
+    ]
+  }
+};
